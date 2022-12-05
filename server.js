639c0f22dd38d6f2831c670b6129b7d773aa3d0f
kkig <kigoshi.kanazawa@gmail.com>
2022-12-05 16:41:58 +0900

    Update project name in package.json

--- server.js
+++ server.js
@@ -1,53 +1,22 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
 
 //END_ASYNC
 
 //START_SYNC
 
-
-
 //END_SYNC
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3000, () => {});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {});
